502dea6af50fb421ef6bcd8cf85db56e47b757bb
Idddd <956020859@qq.com>
2026-05-06 16:11:45 +0800

    Allow fake login stop without pid file

--- fake-login-server/build.gradle.kts
+++ fake-login-server/build.gradle.kts
@@ -5,6 +5,7 @@ import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Classpath
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
@@ -70,8 +71,7 @@ abstract class StartFakeLoginServerTask : DefaultTask() {
 }
 
 abstract class StopFakeLoginServerTask : DefaultTask() {
-    @get:InputFile
-    @get:Optional
+    @get:Internal
     abstract val pidFile: RegularFileProperty
 
     @TaskAction
